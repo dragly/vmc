fe6a9001a9c4b4d2796739ea0e63de916a5332cd
Svenn-Arne Dragly <s@dragly.com>
2012-03-13 19:17:24 +0100

    Some minor updates.

--- wavefunction/wavesimple.h
+++ wavefunction/wavesimple.h
@@ -1,14 +1,19 @@
 #ifndef WAVESIMPLE_H
 #define WAVESIMPLE_H
 
+#include <armadillo>
+
 #include "wavefunction.h"
+
+using namespace std;
+using namespace arma;
 
 class WaveSimple : public WaveFunction
 {
 public:
     WaveSimple(int m_nParticles, int m_nDimensions);
     double wave(double **r);
-    double laplace(double **r);
+    double laplace(vec2 *r);
     void setUseAnalyticalLaplace(bool val);
 private:
     bool useAnalytical;
